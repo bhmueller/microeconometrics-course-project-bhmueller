8063755c2f905e27780e39e6169eeea2bbca39bc
bhmueller <s6bemuel@uni-bonn.de>
2020-07-15 19:30:08 +0200

    Add function table_4.

--- auxiliary/functions.py
+++ auxiliary/functions.py
@@ -114,3 +114,281 @@ def binned_plot(df, bin_num, ylim, years):
         df_bin['Crime rate'] = binned_stats.statistic
         df_bin['Draftnumber'] = bins[1: bin_num+1]
         df_bin.plot.line(x='Draftnumber', y='Crime rate', title=f'Crime Rates for Cohort {i}', ylim=ylim)
+        
+# Regressions (initially for table 4).
+def regress(df, method, cohort_range, cohort_dummies, controls):
+    '''
+    df: data frame to use.
+    method: string, either 'IV' for IV2SLSL by linearmodels or 'OLS' for OLS by statsmodels.
+    cohort_range: list/2-tuple, indicating first and last cohort.
+    cohorts: cohort dummies to include, for 1958-'62: cohorts=cohorts[29: 33].
+    controls: string, either 'y' or 'n'.
+    '''
+    constant, highnumber, conscription, crimerate, malvinas, navy, origin, cohorts, districts, hn_malvinas, df = get_variables()
+    
+    if method == 'OLS':
+        if controls == 'y':
+            vars_controls = highnumber + cohort_dummies + origin + districts + constant
+            df = df[df.cohort >= cohort_range[0]][df.cohort <= cohort_range[1]][vars_controls + crimerate].dropna().copy()
+            X = df[vars_controls].copy()
+            y = df.loc[:, 'crimerate']
+                
+            rslts = sm.OLS(y, X).fit()
+            return rslts
+        
+        if controls == 'n':
+            vars_no_controls = highnumber + cohort_dummies + constant
+            df = df[df.cohort >= cohort_range[0]][df.cohort <= cohort_range[1]][vars_no_controls + crimerate].dropna().copy()
+            X = df[vars_no_controls].copy()
+            y = df.loc[:, 'crimerate']
+                
+            rslts = sm.OLS(y, X).fit()
+            return rslts
+        
+    if method == 'IV':
+        
+        if controls == 'y':
+            cohorts=cohorts[29: 33]
+            vars_controls = highnumber + conscription + cohort_dummies + origin + districts + constant
+            df = df[df.cohort >= cohort_range[0]][df.cohort <= cohort_range[1]][vars_controls + crimerate].copy().dropna(axis=0)
+            y = df.loc[:, 'crimerate']
+            
+            rslts = IV2SLS(y, df[constant + cohorts + origin + districts], df['sm'], df['highnumber']).fit()
+            return rslts
+        
+        if controls == 'n':
+            cohorts=cohorts[29: 33]
+            vars_no_controls = highnumber + conscription + cohort_dummies + constant
+            df = df[df.cohort >= cohort_range[0]][df.cohort <= cohort_range[1]][vars_no_controls + crimerate].copy().dropna(axis=0)
+            y = df.loc[:, 'crimerate']
+            
+            rslts = IV2SLS(y, df[constant + cohorts], df['sm'], df['highnumber']).fit()
+            return rslts
+        
+# Regressions for table 4.
+def regressions_table_4(df):
+    '''
+    Function returns regression results as in table 4 in Galiani et al. 2011.
+    First, it computes the estimates.
+    Arguments:
+    df: data frame to use.
+    '''
+    constant, highnumber, conscription, crimerate, malvinas, navy, origin, cohorts, districts, hn_malvinas, df = get_variables()
+    # Lists to store estimates, standard errors, no. of obs, percent change, and whether controls were used.
+    est_sm = []
+    est_hn = []
+    std_hn = []
+    std_sm = []
+    pval_sm = []
+    pval_hn = []
+    percent_change = []
+    num_obs = []
+    
+    # For computing percent change:
+    p1 = df['sm'][df.cohort >= 1958][df.cohort <= 1962][df.highnumber == 1].dropna().mean()
+    p2 = df['sm'][df.cohort >= 1958][df.cohort <= 1962][df.highnumber == 0].dropna().mean()
+    
+    # Get regressions.
+    # Col 1.
+    rslts = regress(df=df, method='OLS', cohort_range=[1958, 1962], cohort_dummies=cohorts[29: 33], controls='n')
+            
+    est_sm.append('-')
+    est_hn.append(rslts.params['highnumber'])
+    std_sm.append('-')
+    std_hn.append(rslts.HC0_se['highnumber'])
+    pval_sm.append('-')
+    pval_hn.append(rslts.pvalues.highnumber)
+    num_obs.append(rslts.nobs)
+            
+    wald = (rslts.params['highnumber']/(p1 - p2))
+    mean_crime = df.crimerate[df.cohort >= 1958][df.cohort <= 1962][df.highnumber == 0].mean()
+    percent_change.append(100*wald/mean_crime)
+            
+    # Col 2.
+    rslts = regress(df=df, method='OLS', cohort_range=[1958, 1962], cohort_dummies=cohorts[29: 33], controls='y')
+            
+    est_sm.append('-')
+    est_hn.append(rslts.params['highnumber'])
+    std_sm.append('-')
+    std_hn.append(rslts.HC0_se['highnumber'])
+    pval_sm.append('-')
+    pval_hn.append(rslts.pvalues.highnumber)
+    num_obs.append(rslts.nobs)
+            
+    wald = (rslts.params['highnumber']/(p1 - p2))
+    mean_crime = df.crimerate[df.cohort >= 1958][df.cohort <= 1962][df.highnumber == 0].mean()
+    percent_change.append(100*wald/mean_crime)
+            
+    # Col 3.
+    rslts = regress(df=df, method='IV', cohort_range=[1958, 1962], cohort_dummies=cohorts[29: 33], controls='n')
+            
+    est_sm.append(rslts.params['sm'])
+    est_hn.append('-')
+    std_sm.append(rslts.std_errors.sm)
+    std_hn.append('-')
+    pval_sm.append(rslts.pvalues.sm)
+    pval_hn.append('-')
+    num_obs.append(rslts.nobs)
+            
+    mean_crime = df.crimerate[df.cohort >= 1958][df.cohort <= 1962][df.highnumber == 0].mean()
+    percent_change.append(100*rslts.params['sm']/mean_crime)
+            
+    #Col 4.
+    rslts = regress(df=df, method='IV', cohort_range=[1958, 1962], cohort_dummies=cohorts[29: 33], controls='y')
+            
+    est_sm.append(rslts.params['sm'])
+    est_hn.append('-')
+    std_sm.append(rslts.std_errors.sm)
+    std_hn.append('-')
+    pval_sm.append(rslts.pvalues.sm)
+    pval_hn.append('-')
+    num_obs.append(rslts.nobs)
+            
+    mean_crime = df.crimerate[df.cohort >= 1958][df.cohort <= 1962][df.highnumber == 0].mean()
+    percent_change.append(100*rslts.params['sm']/mean_crime)
+    
+    # Col 5.
+    rslts = regress(df=df, method='OLS', cohort_range=[1929, 1965], cohort_dummies=cohorts[0: 36], controls='n')
+            
+    est_sm.append('-')
+    est_hn.append(rslts.params['highnumber'])
+    std_sm.append('-')
+    std_hn.append(rslts.HC0_se['highnumber'])
+    pval_sm.append('-')
+    pval_hn.append(rslts.pvalues.highnumber)
+    num_obs.append(rslts.nobs)
+            
+    wald = (rslts.params['highnumber']/(p1 - p2))
+    mean_crime = df.crimerate[df.cohort >= 1929][df.cohort <= 1965][df.highnumber == 0].mean()
+    percent_change.append(100*wald/mean_crime)
+            
+    # Col 6.
+    rslts = regress(df=df, method='OLS', cohort_range=[1929, 1955], cohort_dummies=cohorts[0: 26], controls='n')
+            
+    est_sm.append('-')
+    est_hn.append(rslts.params['highnumber'])
+    std_sm.append('-')
+    std_hn.append(rslts.HC0_se['highnumber'])
+    pval_sm.append('-')
+    pval_hn.append(rslts.pvalues.highnumber)
+    num_obs.append(rslts.nobs)
+            
+    wald = (rslts.params['highnumber']/(p1 - p2))
+    mean_crime = df.crimerate[df.cohort >= 1929][df.cohort <= 1955][df.highnumber == 0].mean()
+    percent_change.append(100*wald/mean_crime)
+            
+    # Col 7.
+    rslts = regress(df=df, method='OLS', cohort_range=[1958, 1965], cohort_dummies=cohorts[29: 36], controls='n')
+            
+    est_sm.append('-')
+    est_hn.append(rslts.params['highnumber'])
+    std_sm.append('-')
+    std_hn.append(rslts.HC0_se['highnumber'])
+    pval_sm.append('-')
+    pval_hn.append(rslts.pvalues.highnumber)
+    num_obs.append(rslts.nobs)
+            
+    wald = (rslts.params['highnumber']/(p1 - p2))
+    mean_crime = df.crimerate[df.cohort >= 1958][df.cohort <= 1965][df.highnumber == 0].mean()
+    percent_change.append(100*wald/mean_crime)
+            
+    return est_sm, est_hn, std_sm, std_hn, pval_sm, pval_hn, percent_change, num_obs
+
+# Get table 4.
+def table_4(df):
+    '''
+    Function returns table representing table 4 in Galiani et al. 2011.
+    Arguments:
+    df: data frame to use.
+    '''
+    #constant, highnumber, conscription, crimerate, malvinas, navy, origin, cohorts, districts, hn_malvinas, df = get_variables()
+    # Get regression results.
+    est_sm, est_hn, std_sm, std_hn, pval_sm, pval_hn, percent_change, num_obs = regressions_table_4(df)
+    
+    # Print table.
+    print('\033[1m' 'Table 4 - Estimated Impact of Conscription on Crime Rates ' '\033[0m')
+    print(128*'_')
+
+    print('{:<15s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}'\
+          .format('Cohort', '1958-1962', '', "1958-1962", '', "1958-1962", '', "1958-1962", '', "1929-1965", '', "1929-1955", '', \
+                "1958-1965", '', ''))
+
+    print('{:<15s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}'\
+          .format('', '(1)', '', '(2)', '', '(3)', '', '(4)', '', '(5)', '', '(6)', '', \
+                '(7)', '', ''))
+    print(128*'_')
+
+
+    for i in range(len(est_hn)):
+        if i == 0:
+            print('{:<15s}'.format("Draft Eligible"), end="")
+        if type(est_hn[i]) == str:
+            print('{:>13s}{:<3s}'.format('', ''), end="")
+        else:   
+            print('\033[1m' '{:>13.4f}{:<3s}' '\033[0m'.format(est_hn[i], significance(pval_hn[i])), end="")
+    
+    print('\n')
+
+    for i in range(len(std_hn)):
+        if i == 0:
+            print('{:<15s}'.format(''), end="")
+        if type(est_hn[i]) == str:
+            print('{:>13s}{:<3s}'.format('', ''), end="")
+        else:
+            print('{:>13.4f}{:<3s}'.format(std_hn[i], ''), end="")
+        
+    print('\n')
+        
+    for i in range(len(est_sm)):
+        if i == 0:
+            print('{:<15s}'.format("Conscription"), end="")
+        if type(est_sm[i]) == str:
+            print('{:>13s}{:<3s}'.format('', ''), end="")
+        else:   
+            print('\033[1m' '{:>13.4f}{:<3s}' '\033[0m'.format(est_sm[i], significance(pval_sm[i])), end="")
+    
+    print('\n')
+
+    for i in range(len(std_sm)):
+        if i == 0:
+            print('{:<15s}'.format(''), end="")
+        if type(est_sm[i]) == str:
+            print('{:>13s}{:<3s}'.format('', ''), end="")
+        else:
+            print('{:>13.4f}{:<3s}'.format(std_sm[i], ''), end="")
+    
+    print('\n')
+
+    for i in range(len(percent_change)):
+        if i == 0:
+            print('{:<15s}'.format('Percent change'), end="")
+        print('\033[1m' '{:>13.2f}{:<3s}' '\033[0m'.format(percent_change[i], ''), end="")
+    
+    print('\n')
+
+    print('{:<15s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}'\
+          .format('Controls', 'No', '', 'Yes', '', 'No', '', 'Yes', '', 'No', '', 'No', '', 'No', '', ''))
+    
+    print('\n')
+
+    for i in range(len(num_obs)):
+        if i == 0:
+            print('{:<15s}'.format('Observations'), end="")
+        print('\033[1m' '{:>13.0f}{:<3s}' '\033[0m'.format(num_obs[i], ''), end="")
+
+    print('\n')
+    
+    print('{:<15s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}{:<3s}{:>13s}'\
+          .format('Method', 'OLS', '', 'OLS', '', '2SLS', '', '2SLS', '', 'OLS', '', 'OLS', '', 'OLS', '', ''))
+
+    print(128*'_')
+    print('Notes: Robust standard errors are shown below estimates. The level of observation is the cohort-ID number combination. All models')
+    print('All models include cohort dummies. The models in columns 2 and 4 include controls for origin (naturalized or indigenous) and dis-')
+    print('trict (the country is divided in 24 districts). In 2SLS models, the instrument for Conscription is Draft Eligible. Percent change')
+    print('for 2SLS models is calculated as 100 × Estimate/mean crime rate of draft-ineligible men. For intention-to-treat models, percent  ')
+    print('change is reported as 100 × Wald estimate/mean crime rate of draft-ineligible men, where the Wald estimate is calculated as ITT  ')
+    print('estitimate/(p1 − p2), where p1 is the probability of serving in the military among those that are draft-eligible, and p2 is the  ')
+    print('probability of serving in the military among those that are not draft-eligible (since we do not have information on compliance   ')
+    print('rates outside the cohorts of 1958 to 1962, in all cases we use the compliance rates for this period).                            ')
+    print('*** Significant at 1 percent level.')
+    print(' ** Significant at 5 percent level.')
